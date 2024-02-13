cb6dd6c021676c3739971ae55b9eaf153782b16b
MinaLouise <msanford24@basecampcodingacademy>
2024-02-13 09:04:24 -0600

    file changes

--- First_Project_With_Postgres/src/Main.java
+++ First_Project_With_Postgres/src/Main.java
@@ -1,21 +1,35 @@
+import java.sql.*;
+import java.util.Scanner;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
-import java.sql.DriverManager;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
         String url = "jdbc:postgresql://localhost:5432/practice";
         String user = "postgres";
         String password = "Marvin123+";
 
-        try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/practice","postgres", "Marvin123+")){
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/practice", "postgres", "Marvin123+")) {
             System.out.println("successfully Logged in");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+
+            System.err.println("Connection error" + e.getErrorCode());
+            e.printStackTrace();
+
+
         }
+        Functions.BookS();
+
+
+
+
+
 
     }
-    }
+}
+
