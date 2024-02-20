b46350bc0dc8711893b6ac6d92f1d659894c91d4
MinaLouise <msanford24@basecampcodingacademy>
2024-02-20 08:35:58 -0600

    no changes

--- First_Project_With_Postgres/src/Main.java
+++ First_Project_With_Postgres/src/Main.java
@@ -1,13 +1,13 @@
 import java.sql.*;
-import java.util.Scanner;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
 public class Main {
+
     public static void main(String[] args) {
 //        Scanner userInput = new Scanner(System.in);
         String url = DatabaseConfigurations.getUrl();
